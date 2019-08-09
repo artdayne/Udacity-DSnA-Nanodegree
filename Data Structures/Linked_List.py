b7d56437375f7d5f3951dd448a673cf56e98c6c4
nirvana47 <nirvana47@gmail.com>
2019-08-08 22:24:25 -0700

    added print function for linked list

--- Data Structures/Linked_List.py
+++ Data Structures/Linked_List.py
@@ -3,8 +3,17 @@ class Node:
         self.value = value
         self.next = None
 
+def print_linked_list(head):
+    current_node = head
+
+    while current_node is not None:
+        print(current_node.value)
+        current_node = current_node.next
+
 head = Node(2)
 head.next = Node(1)
+head.next.next = Node(4)
+head.next.next.next = Node(3)
+head.next.next.next.next = Node(5)
 
-print(head.value)
-print(head.next.value)
+print_linked_list(head)
