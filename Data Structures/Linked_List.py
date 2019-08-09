5525bf5c9891cc4ad7c93dfc89d15ccff0f9663f
nirvana47 <nirvana47@gmail.com>
2019-08-09 13:15:58 -0700

    Added testing code

--- Data Structures/Linked_List.py
+++ Data Structures/Linked_List.py
@@ -57,7 +57,7 @@ class LinkedList:
         
         
         # TODO: Write function to get size here
-        
+
     def to_list(self):
         
         if self.head is None:
@@ -117,7 +117,54 @@ def test_function(input_list, head):
     except Exception as e:
         print("Fail: "  + e)
         
-        
+## Test your implementation here
+
+# Test prepend
+linked_list = LinkedList()
+linked_list.prepend(1)
+assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+linked_list.append(3)
+linked_list.prepend(2)
+assert linked_list.to_list() == [2, 1, 3], f"list contents: {linked_list.to_list()}"
+    
+# Test append
+linked_list = LinkedList()
+linked_list.append(1)
+assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+linked_list.append(3)
+assert linked_list.to_list() == [1, 3], f"list contents: {linked_list.to_list()}"
+
+# Test search
+linked_list.prepend(2)
+linked_list.prepend(1)
+linked_list.append(4)
+linked_list.append(3)
+assert linked_list.search(1).value == 1, f"list contents: {linked_list.to_list()}"
+assert linked_list.search(4).value == 4, f"list contents: {linked_list.to_list()}"
+
+# Test remove
+linked_list.remove(1)
+assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
+linked_list.remove(3)
+assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+linked_list.remove(3)
+assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
+
+# Test pop
+value = linked_list.pop()
+assert value == 2, f"list contents: {linked_list.to_list()}"
+assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
+
+# Test insert 
+linked_list.insert(5, 0)
+assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(2, 1)
+assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+linked_list.insert(3, 6)
+assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+
+# Test size
+assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"        
 
 input_list = [1, 2, 3, 4, 5, 6]
 head = create_linked_list(input_list)
