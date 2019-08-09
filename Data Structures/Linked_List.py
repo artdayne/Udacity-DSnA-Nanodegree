4bb29d1f708819906f063be37ae8f53e3a4f5bd8
nirvana47 <nirvana47@gmail.com>
2019-08-09 13:20:31 -0700

    Commented out older test code

--- Data Structures/Linked_List.py
+++ Data Structures/Linked_List.py
@@ -105,22 +105,22 @@ def create_linked_list(input_list):
         return head
 
 
-### Test Code
-def test_function(input_list, head):
-    try:
-        if len(input_list) == 0:
-            if head is not None:
-                print("Fail")
-                return
-        for value in input_list:
-            if head.value != value:
-                print("Fail")
-                return
-            else:
-                head = head.next
-        print("Pass")
-    except Exception as e:
-        print("Fail: "  + e)
+### Old Test Code
+# def test_function(input_list, head):
+#     try:
+#         if len(input_list) == 0:
+#             if head is not None:
+#                 print("Fail")
+#                 return
+#         for value in input_list:
+#             if head.value != value:
+#                 print("Fail")
+#                 return
+#             else:
+#                 head = head.next
+#         print("Pass")
+#     except Exception as e:
+#         print("Fail: "  + e)
         
 ## Test your implementation here
 
