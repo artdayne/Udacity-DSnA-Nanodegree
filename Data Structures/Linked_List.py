1c152a02b5bf5025adb441f5270cfd61e4ba2bb9
nirvana47 <nirvana47@gmail.com>
2019-08-09 14:30:29 -0700

    Completed definition of prepend

--- Data Structures/Linked_List.py
+++ Data Structures/Linked_List.py
@@ -10,24 +10,36 @@ class LinkedList:
     def prepend(self, value):
         """ Prepend a value to the beginning of the list. """
         if self.head is None:
+            # print("in pre-pend self.head is none")
             self.head = Node(value)
             return
         
         # Move to the head (the first node)
         newHeadNode = Node(value)
+        # print("newHeadNode value in pre-pend is: {} and its next is {}".format(newHeadNode.value, newHeadNode.next))
         newHeadNode.next = self.head
+        self.head = newHeadNode
+        # print("Self.head value in pre-pend is: {} and the Value is {}".format(self.head.value, value))
+        # print("newHeadNode value in pre-pend is: {} and its next is {}".format(newHeadNode.value, newHeadNode.next))
 
     def append(self, value):
         if self.head is None:
+            # print("in append self.head is none")
             self.head = Node(value)
             return
         
         # Move to the tail (the last node)
         node = self.head
+        # print("Node value before loop in append: {}".format(node.value))
+
+        counter = 0
         while node.next:
+            counter += 1
             node = node.next
-        
+        # print("Counter value in append: {}".format(counter))
+
         node.next = Node(value)
+        # print("Value assigned to latest appended node: {}".format(node.next.value))
         return
 
     def search(self, value):
@@ -64,7 +76,6 @@ class LinkedList:
         # TODO: Write function to get size here
 
     def to_list(self):
-        
         if self.head is None:
             return None
         
@@ -130,55 +141,57 @@ linked_list.prepend(1)
 assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
 linked_list.append(3)
 linked_list.prepend(2)
+# linked_list.prepend(7)
+# print(linked_list.to_list())
 assert linked_list.to_list() == [2, 1, 3], f"list contents: {linked_list.to_list()}"
     
-# Test append
-linked_list = LinkedList()
-linked_list.append(1)
-assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
-linked_list.append(3)
-assert linked_list.to_list() == [1, 3], f"list contents: {linked_list.to_list()}"
+# # Test append
+# linked_list = LinkedList()
+# linked_list.append(1)
+# assert linked_list.to_list() == [1], f"list contents: {linked_list.to_list()}"
+# linked_list.append(3)
+# assert linked_list.to_list() == [1, 3], f"list contents: {linked_list.to_list()}"
 
-# Test search
-linked_list.prepend(2)
-linked_list.prepend(1)
-linked_list.append(4)
-linked_list.append(3)
-assert linked_list.search(1).value == 1, f"list contents: {linked_list.to_list()}"
-assert linked_list.search(4).value == 4, f"list contents: {linked_list.to_list()}"
+# # Test search
+# linked_list.prepend(2)
+# linked_list.prepend(1)
+# linked_list.append(4)
+# linked_list.append(3)
+# assert linked_list.search(1).value == 1, f"list contents: {linked_list.to_list()}"
+# assert linked_list.search(4).value == 4, f"list contents: {linked_list.to_list()}"
 
-# Test remove
-linked_list.remove(1)
-assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
-linked_list.remove(3)
-assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
-linked_list.remove(3)
-assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
+# # Test remove
+# linked_list.remove(1)
+# assert linked_list.to_list() == [2, 1, 3, 4, 3], f"list contents: {linked_list.to_list()}"
+# linked_list.remove(3)
+# assert linked_list.to_list() == [2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+# linked_list.remove(3)
+# assert linked_list.to_list() == [2, 1, 4], f"list contents: {linked_list.to_list()}"
 
-# Test pop
-value = linked_list.pop()
-assert value == 2, f"list contents: {linked_list.to_list()}"
-assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
+# # Test pop
+# value = linked_list.pop()
+# assert value == 2, f"list contents: {linked_list.to_list()}"
+# assert linked_list.head.value == 1, f"list contents: {linked_list.to_list()}"
 
-# Test insert 
-linked_list.insert(5, 0)
-assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
-linked_list.insert(2, 1)
-assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
-linked_list.insert(3, 6)
-assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
+# # Test insert 
+# linked_list.insert(5, 0)
+# assert linked_list.to_list() == [5, 1, 4], f"list contents: {linked_list.to_list()}"
+# linked_list.insert(2, 1)
+# assert linked_list.to_list() == [5, 2, 1, 4], f"list contents: {linked_list.to_list()}"
+# linked_list.insert(3, 6)
+# assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.to_list()}"
 
-# Test size
-assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"        
+# # Test size
+# assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"        
 
-input_list = [1, 2, 3, 4, 5, 6]
-head = create_linked_list(input_list)
-test_function(input_list, head)
+# input_list = [1, 2, 3, 4, 5, 6]
+# head = create_linked_list(input_list)
+# test_function(input_list, head)
 
-input_list = [1]
-head = create_linked_list(input_list)
-test_function(input_list, head)
+# input_list = [1]
+# head = create_linked_list(input_list)
+# test_function(input_list, head)
 
-input_list = []
-head = create_linked_list(input_list)
-test_function(input_list, head)
+# input_list = []
+# head = create_linked_list(input_list)
+# test_function(input_list, head)
