98090c1f54624a198fd37a9b80e72f8d7325f702
nirvana47 <nirvana47@gmail.com>
2019-08-11 15:28:35 -0700

    Added iter, repr and definition for reverse LL

--- Data Structures/Linked_List.py
+++ Data Structures/Linked_List.py
@@ -7,6 +7,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.value
+            node = node.next
+            
+    def __repr__(self):
+        return str([v for v in self])
+        
     def prepend(self, value):
         """ Prepend a value to the beginning of the list. """
         if self.head is None:
@@ -149,7 +158,19 @@ def reverse(linked_list):
     Returns:
        obj: Reveresed Linked List
     """
-    
+    new_list = LinkedList()
+    node = linked_list.head
+    prev_node = None
+
+    # A bit of a complex operation here. We want to take the
+    # node from the original linked list and prepend it to 
+    # the new linked list
+    for value in linked_list:
+        new_node = Node(value)
+        new_node.next = prev_node
+        prev_node = new_node
+    new_list.head = prev_node
+    return new_list
     
 ## Test your implementation here
 
@@ -201,6 +222,13 @@ assert linked_list.to_list() == [5, 2, 1, 4, 3], f"list contents: {linked_list.t
 assert linked_list.size() == 5, f"list contents: {linked_list.to_list()}"        
 print(linked_list.to_list())
 
+llist = LinkedList()
+for value in [4,2,5,1,-3,0]:
+    llist.append(value)
+
+flipped = reverse(llist)
+is_correct = list(flipped) == list([0,-3,1,5,2,4]) and list(llist) == list(reverse(flipped))
+print("Pass" if is_correct else "Fail")
 # input_list = [1, 2, 3, 4, 5, 6]
 # head = create_linked_list(input_list)
 # test_function(input_list, head)
