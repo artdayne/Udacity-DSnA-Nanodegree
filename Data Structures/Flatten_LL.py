f5a43efe712e0647c3167b57f4f120b1df0f67cc
nirvana47 <nirvana47@gmail.com>
2019-08-11 17:41:23 -0700

    added definition for merge method

--- Data Structures/Flatten_LL.py
+++ Data Structures/Flatten_LL.py
@@ -34,8 +34,27 @@ def merge(list1, list2):
     Returns:
        linked-list: Merged and sorted linked-list, a combination of list1 and list2
     """
-    # TODO: Implement this function so that it merges the two linked lists in a single, sorted linked list.
-    pass
+    merged = LinkedList(None)
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+    list1_elt = list1.head
+    list2_elt = list2.head
+    while list1_elt is not None or list2_elt is not None:
+        if list1_elt is None:
+            merged.append(list2_elt)
+            list2_elt = list2_elt.next
+        elif list2_elt is None:
+            merged.append(list1_elt)
+            list1_elt = list1_elt.next
+        elif list1_elt.value <= list2_elt.value:
+            merged.append(list1_elt)
+            list1_elt = list1_elt.next
+        else:
+            merged.append(list2_elt)
+            list2_elt = list2_elt.next
+    return merged
 
 class NestedLinkedList(LinkedList):
     def flatten(self):
