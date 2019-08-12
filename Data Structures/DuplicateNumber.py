b7f91c993d18769d45b888cd444a539e561ca147
nirvana47 <nirvana47@gmail.com>
2019-08-11 22:44:13 -0700

    Added definition for duplicate_number

--- Data Structures/DuplicateNumber.py
+++ Data Structures/DuplicateNumber.py
@@ -3,7 +3,15 @@ def duplicate_number(arr):
     :param - array containing numbers in the range [0, len(arr) - 2]
     return - the number that is duplicate in the arr
     """
-    pass
+    current_sum = 0
+    expected_sum = 0
+    
+    for num in arr:
+        current_sum += num
+        
+    for i in range(len(arr) - 1):
+        expected_sum += i
+    return current_sum - expected_sum
 
 def test_function(test_case):
     arr = test_case[0]
