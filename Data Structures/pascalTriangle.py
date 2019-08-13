5e996b1c5d5a4b013cb96e9762834699f4c3f82e
nirvana47 <nirvana47@gmail.com>
2019-08-12 21:03:12 -0700

    Added method declaration and test cases

---
+++ Data Structures/pascalTriangle.py
@@ -0,0 +1,49 @@
+# Find and return the nth row of Pascal's triangle in the form a list. n is 0-based.
+# For exmaple, if n = 4, then output = [1, 4, 6, 4, 1].
+# To know more about Pascal's triangle: https://www.mathsisfun.com/pascals-triangle.html
+
+def nth_row_pascal(n):
+    """
+    :param: - n - index (0 based)
+    return - list() representing nth row of Pascal's triangle
+    """
+
+
+def test_function(test_case):
+    n = test_case[0]
+    solution = test_case[1]
+    output = nth_row_pascal(n)
+    if solution == output:
+        print("Pass")
+    else:
+        print("Fail")
+
+n = 0
+solution = [1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 1
+solution = [1, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 2
+solution = [1, 2, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 3
+solution = [1, 3, 3, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+
+n = 4
+solution = [1, 4, 6, 4, 1]
+
+test_case = [n, solution]
+test_function(test_case)
