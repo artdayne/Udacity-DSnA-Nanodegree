4314f38363883cf1b4bfb9e18a1b9a47888c6600
nirvana47 <nirvana47@gmail.com>
2019-08-12 20:48:31 -0700

    My solution feels more resource hogging

--- Data Structures/MaxSumSubarray.py
+++ Data Structures/MaxSumSubarray.py
@@ -15,25 +15,28 @@ def max_sum_subarray(arr):
     :param - arr - input array
     return - number - largest sum in contiguous subarry within arr
     """
-    # sumArray = sum(arr)
-    # maxSum = sumArray
-    # while len(arr) > 1:
-    #     if arr[0] > arr[len(arr)-1]:
-    #         arr = arr[:-1]
-    #     else:
-    #         arr = arr[1:]
-    #     if sum(arr) > maxSum:
-    #         maxSum = sum(arr)
-    # return maxSum
+    sumArray = sum(arr)
+    maxSum = sumArray
+    while len(arr) > 1:
+        if arr[0] > arr[len(arr)-1]:
+            arr = arr[:-1]
+        else:
+            arr = arr[1:]
+        if sum(arr) > maxSum:
+            maxSum = sum(arr)
+    return maxSum
 
     # def max_sum_subarray(arr):
-    max_sum = arr[0]
-    current_sum = arr[0]
+    # max_sum = arr[0]
+    # current_sum = arr[0]
 
-    for num in arr[1:]:
-        current_sum = max(current_sum + num, num)
-        max_sum = max(current_sum, max_sum)
-    return max_sum
+    # print("BEFORE IT ALL BEGINS | Array is {}, max_sum is {}, current_sum is {}". format(arr, max_sum, current_sum))
+
+    # for num in arr[1:]:
+    #     current_sum = max(current_sum + num, num)
+    #     max_sum = max(current_sum, max_sum)
+    #     print("num is {}, max_sum is {}, current_sum is {}". format(num, max_sum, current_sum))
+    # return max_sum
 
 def test_function(test_case):
     arr = test_case[0]
@@ -59,6 +62,12 @@ test_function(test_case)
 
 arr = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
 solution = 18  # sum of subarray = [15, -13, 14, -1, 2, 1]
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [-1,100,-80]
+solution = 100  # sum of subarray = [15, -13, 14, -1, 2, 1]
 
 test_case = [arr, solution]
 test_function(test_case)
