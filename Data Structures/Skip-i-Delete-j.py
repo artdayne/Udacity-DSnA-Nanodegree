c1078e1159ceb3f2ab58fef53da9febcd7f15ab5
nirvana47 <nirvana47@gmail.com>
2019-08-14 21:34:27 -0700

    Added problem description

---
+++ Data Structures/Skip-i-Delete-j.py
@@ -0,0 +1,10 @@
+# You are given the head of a linked list and two integers, i and j. 
+# You have to retain the first i nodes and then delete the next j nodes. 
+# Continue doing so until the end of the linked list.
+
+# Example:
+# linked-list = 1 2 3 4 5 6 7 8 9 10 11 12
+# i = 2
+# j = 3
+# Output = 1 2 6 7 11 12
+
